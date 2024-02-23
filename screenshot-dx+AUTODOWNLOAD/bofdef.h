00df18a67914e6589826ddd237f9e7717cdfd6a1
Ed <59506742+0xflagplz@users.noreply.github.com>
2024-02-23 09:58:28 -0700

    Update bofdef.h

--- screenshot-dx+AUTODOWNLOAD/bofdef.h
+++ screenshot-dx+AUTODOWNLOAD/bofdef.h
@@ -2,6 +2,8 @@
 #include <d3d9.h>
 #include <stdio.h>
 
+DECLSPEC_IMPORT HRESULT WINAPI OLE32$CoInitializeEx(LPVOID pvReserved, DWORD dwCoInit);
+DECLSPEC_IMPORT void WINAPI OLE32$CoUninitialize(void);
 WINBASEAPI void __cdecl MSVCRT$free(void* memblock);
 WINBASEAPI void* __cdecl MSVCRT$malloc(size_t _Size);
 DECLSPEC_IMPORT char* WINAPI MSVCRT$getenv(const wchar_t *varname);
@@ -14,5 +16,7 @@ WINBASEAPI int WINAPI MSVCRT$sprintf(char* buffer, const char* format, ...);
 WINBASEAPI int __cdecl MSVCRT$rand();
 WINBASEAPI size_t WINAPI MSVCRT$strlen(const char* str);
 WINBASEAPI void __cdecl MSVCRT$srand(int initial);
-WINBASEAPI time_t __cdecl MSVCRT$time(time_t *time);
+WINBASEAPI size_t __cdecl MSVCRT$strftime(char* strDest, size_t maxsize, const char* format, const struct tm* timeptr);
+WINBASEAPI time_t __cdecl MSVCRT$time(time_t* timer);
+WINBASEAPI struct tm* __cdecl MSVCRT$localtime(const time_t* _Time);
 DECLSPEC_IMPORT IDirect3D9* WINAPI D3D9$Direct3DCreate9(UINT SDKVersion);
